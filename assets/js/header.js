28436a0e01d479ab3116061687da392b663f9ccd
Mauro Sebastian Ferraresi <msferraresi@gmail.com>
2023-10-11 17:13:09 -0300

    Se realizaron cambioos sobre el header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,9 @@
-// Agrega un evento click al icono de hamburguesa para mostrar/ocultar el menú
-const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
-const navList = document.querySelector('nav ul.nav-list');
-
-mobileMenuToggle.addEventListener('click', function () {
+function toggleMobileMenu() {
+    var navList = document.querySelector('nav ul.nav-list');
     if (navList.style.display === 'block' || navList.style.display === '') {
         navList.style.display = 'none';
     } else {
         navList.style.display = 'block';
     }
-});
+}
+
