5fd250e67c62862a7f45f3a49e6210783f4ab7da
fedemquinteros <federicomanuelquinteros@gmail.com>
2023-11-09 01:46:06 -0300

    primer agregado de una api al sitio, necesita mejoras

--- assets/js/paginaprincipal.js
+++ assets/js/paginaprincipal.js
@@ -1,5 +1,47 @@
-function cerrarSesion(){
-    localStorage.removeItem("nombreSesion");
-    localStorage.removeItem("passSesion");
-    cargarPagina('home')
+
+function cerrarSesion() {
+  localStorage.removeItem("nombreSesion");
+  localStorage.removeItem("passSesion");
+  cargarPagina('home');
 }
+
+
+function obtenerListaDeLigas() {
+  const apiUrl = 'https://www.thesportsdb.com/api/v1/json/3/all_leagues.php';
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const listaLigas = document.getElementById('listaLigas');
+      let ligaArgentinaLink;
+
+      data.leagues.forEach(league => {
+        const listItem = document.createElement('li');
+        listItem.textContent = league.strLeague;
+        listaLigas.appendChild(listItem);
+
+        
+        if (league.strLeague === 'Argentina Primera Division') {
+          ligaArgentinaLink = league.strWebsite;
+        }
+      });
+
+     
+      if (ligaArgentinaLink) {
+        const linkItem = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = ligaArgentinaLink;
+        link.textContent = 'Enlace a la liga argentina de fútbol';
+        linkItem.appendChild(link);
+        listaLigas.appendChild(linkItem);
+      }
+    })
+    .catch(error => {
+      console.error('Error al obtener la lista de ligas', error);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  obtenerListaDeLigas();
+});
