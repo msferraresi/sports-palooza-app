91a7cc6ee7f74b964d432d1f5c7bb78b182d252c
fedemquinteros <federicomanuelquinteros@gmail.com>
2023-11-10 12:47:29 -0300

    ultima modificacion de la pagina principal tanto html como js, agregado de funcionalidad y llamada a api

--- assets/js/paginaprincipal.js
+++ assets/js/paginaprincipal.js
@@ -1,44 +1,15 @@
-
+//primer API
 document.addEventListener('DOMContentLoaded', function () {
-  const apiKey = '3b74ae80bbmsh9a1a268dad8d097p100222jsnd0e0d5aec999';
-  const locationKey = 'Santa Isabel, Córdoba';
+  const apiUrl = 'https://www.freeforexapi.com/api/live';
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': apiKey,
-      'X-RapidAPI-Host': 'accuweatherstefan-skliarovv1.p.rapidapi.com'
-    }
-  };
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+     
+      console.log(data);
 
-  // URL de la API para obtener condiciones climáticas por ubicación.
-  const apiUrl = `https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey?locationKey=${locationKey}`;
-
-  const weatherInfo = document.getElementById('weather-info');
-
-  async function obtenerCondicionesClimaticas() {
-    try {
-      const response = await fetch(apiUrl, options);
-      const data = await response.json();
-
-      // Manipula los datos y muestra la información en la página.
-      if (data && data.length > 0) {
-        const ul = document.createElement('ul');
-        data.forEach(entry => {
-          const li = document.createElement('li');
-          li.textContent = `Hora: ${entry.DateTime}, Temperatura: ${entry.Temperature.Imperial.Value} °F`;
-          ul.appendChild(li);
-        });
-        weatherInfo.appendChild(ul);
-      } else {
-        weatherInfo.textContent = 'No se encontraron datos climáticos.';
-      }
-    } catch (error) {
-      console.error(error);
-      weatherInfo.textContent = 'Error al cargar los datos climáticos.';
-    }
-  }
-
-  // Llama a la función para obtener condiciones climáticas cuando la página se carga.
-  obtenerCondicionesClimaticas();
+      const eurUsdPrice = data.rates.USDEUR.rate;
+      console.log(`El precio de EUR/USD es: ${eurUsdPrice}`);
+    })
+    .catch(error => console.error('Error al obtener datos:', error));
 });
